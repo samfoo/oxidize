73748b43277e89b340109eef03ad4fe1a8575545
Sam Gibson <sam@ifdown.net>
2015-08-26 19:18:58 +1200

    Change to hamcrest-y style

--- src/lib.rs
+++ src/lib.rs
@@ -2,54 +2,64 @@
 extern crate core;
 use core::fmt::Debug;
 
-pub struct Expectation<Lhs: Debug> {
-    to: To<Lhs>
-}
+pub struct Expectation<Lhs: Debug>(Lhs);
 
 pub struct To<Lhs: Debug>(Lhs);
 
-impl<Lhs: Debug> To<Lhs> {
-    fn be<T: Debug>(&self, rhs: T) -> &Self where Lhs: PartialEq<T> {
-        self.equal(rhs)
+pub trait Matcher<Lhs: Debug> {
+    fn matches(&self, rhs: &Lhs) -> bool;
+    fn fail_msg(&self, rhs: &Lhs) -> String;
+}
+
+pub struct Equal<Lhs: Debug>(Lhs);
+
+impl<Lhs: Debug + PartialEq> Matcher<Lhs> for Equal<Lhs> {
+    fn matches(&self, rhs: &Lhs) -> bool {
+        &self.0 == rhs
     }
 
-    fn equal<T: Debug>(&self, rhs: T) -> &Self where Lhs: PartialEq<T> {
-        if self.0 != rhs {
-            panic!("expected {:?} to equal {:?}", self.0, rhs);
+    fn fail_msg(&self, rhs: &Lhs) -> String {
+        format!("expected {:?} to equal {:?}", rhs, self.0)
+    }
+}
+
+impl<Lhs: Debug> Expectation<Lhs> {
+    fn to<T>(&self, matcher: T) where T: Matcher<Lhs> {
+        if !matcher.matches(&self.0) {
+            panic!(matcher.fail_msg(&self.0))
         }
-        &self
     }
 }
 
 pub fn expect<T: Debug>(lhs: T) -> Expectation<T> {
-    Expectation {
-        to: To(lhs)
-    }
+    Expectation(lhs)
 }
 
+pub fn equal<T: Debug>(rhs: T) -> Equal<T> {
+    Equal(rhs)
+}
+
+#[cfg(test)]
 mod test {
-    #[test]
-    fn test_expect_equal_int_to_pass() {
-        super::expect(1).to.equal(1);
-    }
+    use super::{expect, equal};
 
     #[test]
-    fn test_expect_be_int_to_pass() {
-        super::expect(1).to.be(1);
+    fn test_expect_equality() {
+        expect(1).to(equal(1));
     }
 
     #[test]
     #[should_panic(expected="expected 1 to equal 2")]
-    fn test_expect_not_equal_int_to_fail() {
-        super::expect(1).to.equal(2);
+    fn test_expect_not_equality_to_fail() {
+        expect(1).to(equal(2));
     }
 
     #[test]
-    fn test_struct_cmp() {
+    fn test_partial_eq_equality() {
         #[derive(PartialEq, Debug)]
         struct Foo { a: u16, b: String }
 
-        super::expect(Foo{ a: 10u16, b: "Hello".to_string()})
-            .to.equal(Foo{ a: 10u16, b: "Hello".to_string()});
+        expect(Foo{ a: 10u16, b: "Hello".to_string()})
+            .to(equal(Foo{ a: 10u16, b: "Hello".to_string()}));
     }
 }
