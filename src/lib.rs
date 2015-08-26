5050d507b8b8e39a3aae3a34af5bf6753b0d4c39
Sam Gibson <sam@ifdown.net>
2015-08-26 19:59:08 +1200

    Implement `not` matcher

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 #![feature(core)]
 extern crate core;
 use core::fmt::Debug;
+use std::marker::PhantomData;
 
 pub struct Expectation<Lhs: Debug>(Lhs);
 
 pub struct To<Lhs: Debug>(Lhs);
 
-pub trait Matcher<Lhs: Debug> {
+pub trait Matcher<Lhs> {
     fn matches(&self, rhs: &Lhs) -> bool;
     fn fail_msg(&self, rhs: &Lhs) -> String;
 }
@@ -20,6 +21,18 @@ impl<Lhs: Debug + PartialEq> Matcher<Lhs> for Equal<Lhs> {
 
     fn fail_msg(&self, rhs: &Lhs) -> String {
         format!("expected {:?} to equal {:?}", rhs, self.0)
+    }
+}
+
+pub struct Not<A, M>(M, PhantomData<A>) where A: Debug, M: Matcher<A>;
+
+impl<Lhs: Debug, M: Matcher<Lhs>> Matcher<Lhs> for Not<Lhs, M> {
+    fn matches(&self, rhs: &Lhs) -> bool {
+        !self.0.matches(rhs)
+    }
+
+    fn fail_msg(&self, rhs: &Lhs) -> String {
+        format!("not {}", self.0.fail_msg(rhs))
     }
 }
 
@@ -39,9 +52,13 @@ pub fn equal<T: Debug>(rhs: T) -> Equal<T> {
     Equal(rhs)
 }
 
+pub fn not<T: Debug, M: Matcher<T>>(rhs: M) -> Not<T, M> {
+    Not(rhs, PhantomData)
+}
+
 #[cfg(test)]
 mod test {
-    use super::{expect, equal};
+    use super::{expect, equal, not};
 
     #[test]
     fn test_expect_equality() {
@@ -61,5 +78,21 @@ mod test {
 
         expect(Foo{ a: 10u16, b: "Hello".to_string()})
             .to(equal(Foo{ a: 10u16, b: "Hello".to_string()}));
+    }
+
+    #[test]
+    fn test_invert_equality() {
+        expect(1).to(not(equal(2)));
+    }
+
+    #[test]
+    fn test_invert_invert_equality() {
+        expect(1).to(not(not(equal(1))));
+    }
+
+    #[test]
+    #[should_panic(expected="not expected 1 to equal 1")]
+    fn test_invert_equality_equal_to_fail() {
+        expect(1).to(not(equal(1)));
     }
 }
