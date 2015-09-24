d3ce0494dd74bbb8696307ac62214729a7901359
Sam Gibson <sam@ifdown.net>
2015-09-24 17:52:09 +1200

    Add regex matcher for String

--- src/matchers/regex.rs
+++ src/matchers/regex.rs
@@ -20,24 +20,64 @@ impl<'a> Matcher<&'a str> for MatchesRegex<'a> {
     }
 }
 
+impl<'a> Matcher<String> for MatchesRegex<'a> {
+    fn matches(&self, lhs: &String) -> bool {
+        match Regex::new(self.0) {
+            Ok(re) => re.is_match(&*lhs),
+            Err(_) => false
+        }
+    }
+
+    fn fail_msg(&self, lhs: &String) -> String {
+        format!("expected {:?} to match {:?}", lhs, self.0)
+    }
+
+    fn negated_fail_msg(&self, lhs: &String) -> String {
+        format!("expected {:?} not to match {:?}", lhs, self.0)
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use super::super::super::dsl::*;
+    mod str_match_regex {
+        use super::super::super::super::dsl::*;
 
-    #[test]
-    fn test_match_regex_matches() {
-        expect("Hello").to(match_regex(r"..ll."))
+        #[test]
+        fn test_match_regex_matches() {
+            expect("Hello").to(match_regex(r"..ll."))
+        }
+
+        #[test]
+        #[should_panic(expected="expected \"Hello\" to match \".\\\\o/.\"")]
+        fn test_match_regex_fails_with_message() {
+            expect("Hello").to(match_regex(".\\o/."))
+        }
+
+        #[test]
+        #[should_panic(expected="expected \"-\\\\o/-\" not to match \".....\"")]
+        fn test_negated_match_regex_fails_with_message() {
+            expect("-\\o/-").to_not(match_regex("....."))
+        }
     }
 
-    #[test]
-    #[should_panic(expected="expected \"Hello\" to match \".\\\\o/.\"")]
-    fn test_match_regex_fails_with_message() {
-        expect("Hello").to(match_regex(".\\o/."))
-    }
+    mod string_match_regex {
+        use super::super::super::super::dsl::*;
 
-    #[test]
-    #[should_panic(expected="expected \"-\\\\o/-\" not to match \".....\"")]
-    fn test_negated_match_regex_fails_with_message() {
-        expect("-\\o/-").to_not(match_regex("....."))
+        #[test]
+        fn test_match_regex_matches() {
+            expect("Hello".to_string()).to(match_regex(r"..ll."))
+        }
+
+        #[test]
+        #[should_panic(expected="expected \"Hello\" to match \".\\\\o/.\"")]
+        fn test_match_regex_fails_with_message() {
+            expect("Hello".to_string()).to(match_regex(".\\o/."))
+        }
+
+        #[test]
+        #[should_panic(expected="expected \"-\\\\o/-\" not to match \".....\"")]
+        fn test_negated_match_regex_fails_with_message() {
+            expect("-\\o/-".to_string()).to_not(match_regex("....."))
+        }
     }
 }
